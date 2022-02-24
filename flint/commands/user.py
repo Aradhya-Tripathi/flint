5e2672bbabc473d211dd85b5b7ac515edf390536
Aradhya <aradhyatripathi51@gmail.com>
2022-02-25 01:49:36 +0530

    ✨  Added  support for user account removal & whoami

--- flint/commands/user.py
+++ flint/commands/user.py
@@ -1,9 +1,11 @@
+import click
 import httpx
 from flint import domain, protocal
 from flint.hooks import on_failure
 from flint.user import User
 from flint.utils.auth import TokenAuth
 from flint.utils.data import dump_json
+from flint.utils.log import exit_with_message
 
 user = User()
 
@@ -21,3 +23,25 @@ user_details = "user/details"
 def get_data(file):
     response = user_client.get(user_details).json()
     dump_json(response)
+
+
+def remove_account(force):
+    msg = f"Removed user account for {user}"
+    if force:
+        user_client.delete(user_details)
+        user.delete()
+        exit_with_message(msg=msg, fg="yellow", error_code=1)
+
+    if click.confirm(f"Do you want to remove account for {user}"):
+        user_client.delete(user_details)
+        user.delete()
+        exit_with_message(msg=msg, fg="yellow", error_code=1)
+
+
+def whoami(verbose):
+    if verbose:
+        exit_with_message(
+            f"""Currenty logged in as {user}\n{user.whoami}""", fg="yellow"
+        )
+
+    exit_with_message(f"Currently logged in as {user}", fg="yellow")
