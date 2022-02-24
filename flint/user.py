4635aecfa2f8d2981ad877eea2c9ec3c4e2aefc3
Aradhya <aradhyatripathi51@gmail.com>
2022-02-25 01:50:16 +0530

    feat: Added support for whoami

--- flint/user.py
+++ flint/user.py
@@ -6,13 +6,25 @@ from flint.utils.log import exit_with_message
 class User:
     def __init__(self):
         self.netrc = Netrc()
+        try:
+            self.user = self.netrc.hosts[domain]
+        except KeyError:
+            exit_with_message("Please login or register first!", fg="yellow")
 
     @property
     def tokens(self):
-        return self.user()[2]
+        return self.user[2]
 
-    def user(self):
-        try:
-            return self.netrc.hosts[domain]
-        except KeyError:
-            exit_with_message("Please login or register first!", fg="yellow")
+    @property
+    def whoami(self):
+        return f"User name: {self.user[1]} \t Email: {self.user[0]}"
+
+    def delete(self):
+        del self.netrc.hosts[domain]
+        self.netrc.save()
+
+    def __repr__(self) -> str:
+        return self.user[1]
+
+    def __str__(self) -> str:
+        return self.user[1]
