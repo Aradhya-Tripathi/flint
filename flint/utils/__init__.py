ad05894ce879b56b0cb7bae0e15786f241a28ace
Aradhya <aradhyatripathi51@gmail.com>
2022-02-28 23:26:16 +0530

    feat: Added support for verbose exit message

--- flint/utils/__init__.py
+++ flint/utils/__init__.py
@@ -5,6 +5,7 @@ import click
 
 
 class _dict(dict):
+    @property
     def json_data(self):
         return json.dumps(self)
 
@@ -13,3 +14,13 @@ def exit_with_message(msg, fg=None, bg=None, error_code=-1):
     click.secho(msg, fg=fg, bg=bg, err=True)
 
     sys.exit(error_code)
+
+
+def _exit(ctx, msg, **kwargs):
+    if ctx.obj["verbose"]:
+        if response := kwargs.get("response"):
+            msg += f"\nStatus Code: {response.status_code}, Json Response: {response.json()}"
+
+    exit_with_message(
+        msg=msg, fg=kwargs.get("fg", "yellow"), error_code=kwargs.get("error_code")
+    )
